54ef36337de4b37c28cba147c7c1a7da7ab0e42a
kalilventura <kalilventur@gmail.com>
2024-08-23 00:55:57 -0300

    feat(application): add a new backend application
    Signed-off-by: kalilventura <kalilventur@gmail.com>

---
+++ app/backend/pizzaria/src/main/pizzas/Pizza.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.main.todos;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
